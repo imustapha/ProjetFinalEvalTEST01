5b0bb6c7b95119a7fd12a0e1fbce216bc1283f94
igherra <igherra.mustapha@gmail.com>
2016-04-26 09:37:40 +0000

    premiere commit

---
+++ source/ProjetFinalEval/Controllers/CollaborateurTitulaireController.cs
@@ -0,0 +1,209 @@
+﻿using ProjetFinalEval;
+using ProjetFinalEval.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using MySql.AspNet.Identity;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Owin;
+
+namespace AppGestionEvaluation.Controllers
+{
+    public class CollaborateurTitulaireController : Controller
+    {
+        private ApplicationUserManager _userManager;
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+        private async Task SignInAsync(ApplicationUser user, bool isPersistent)
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie);
+            AuthenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = isPersistent }, await user.GenerateUserIdentityAsync(UserManager));
+        }
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        bd_evaluationEntities3 bd = new bd_evaluationEntities3();
+        // GET: Collaborateur
+        public ActionResult Index()
+        {
+            return View(bd.collaborateurtitulaire.ToList());
+        }
+
+        // GET: Collaborateur/Details/5
+        public ActionResult Details(int? id)
+        {
+            ViewBag.Evaluateur = bd.collaborateurtitulaire.Where(m => m.IDCOLLABORATEURTITULAIRE == id).FirstOrDefault().FLAGEVAL;
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            collaborateurtitulaire CollT = bd.collaborateurtitulaire.Find(id);
+            if (CollT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(CollT);
+        }
+        [HttpGet]
+        // GET: Collaborateur/Create
+        public ActionResult Create()
+        {
+            ViewBag.IDFONCTION = new SelectList(bd.fonction, "IDFONCTION", "NOMFONCTION");
+            return View();
+        }
+
+        // POST: Collaborateur/Create
+        [HttpPost]
+        public async Task<ActionResult> Create( RegisterViewModel model)
+        {
+
+            try
+            {
+                if (model.File.ContentLength > (2 * 1024 * 1024))
+                {
+                    ModelState.AddModelError("CustomError", "File est plus 2mb");
+                    return View();
+                }
+                if (!(model.File.ContentType == "image/jpeg" || model.File.ContentType == "image/gif"))
+                {
+                    ModelState.AddModelError("CustomError", "File alloued for jpeg and gif");
+                    return View();
+
+                }
+                byte[] data = new byte[model.File.ContentLength];
+                model.File.InputStream.Read(data, 0, model.File.ContentLength);
+                model.IMAGE = data;
+
+                if (ModelState.IsValid)
+                {
+                var user = new ApplicationUser() { UserName = model.Email, Email = model.Email };
+                var resulte = await UserManager.CreateAsync(user, model.Password);
+
+                var userToInsert =bd.aspnetusers.Where(i=>i.UserName==user.UserName).FirstOrDefault(); 
+                    if(userToInsert != null){
+
+                        var CollT = new collaborateurtitulaire() { IdUser = userToInsert.Id, NOM = model.NOM, PRENOM = model.PRENOM, IDFONCTION = model.IDFONCTION, IMAGE = model.IMAGE, FLAGEVAL = model.FLAGEVAL };
+                        if (resulte.Succeeded)
+                        {
+
+                            await SignInAsync(user, isPersistent: false);
+                            bd.collaborateurtitulaire.Add(CollT);
+                            bd.SaveChanges();
+                            return RedirectToAction("Index");
+
+                        }
+                    }
+                 
+               } 
+                else if (!ModelState.IsValid)
+                {
+                    ViewBag.IDFONCTION = new SelectList(bd.fonction, "IDFONCTION", "NOMFONCTION");
+                    return View();
+                }
+            
+            }
+            catch (Exception ex)
+            {
+                ViewBag.IDFONCTION = new SelectList(bd.fonction, "IDFONCTION", "NOMFONCTION");
+                string tst = ex.Message;
+                return View();
+            }
+            ViewBag.IDFONCTION = new SelectList(bd.fonction, "IDFONCTION", "NOMFONCTION");
+            return View();
+        }
+
+        // GET: Collaborateur/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            ViewBag.IDFONCTION = new SelectList(bd.fonction, "IDFONCTION", "NOMFONCTION", bd.collaborateurtitulaire.Find(id).fonction.IDFONCTION);
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            collaborateurtitulaire CollT = bd.collaborateurtitulaire.Find(id);
+            if (CollT == null)
+                return HttpNotFound();
+            return View(CollT);
+        }
+
+        // POST: Collaborateur/Edit/5
+        [HttpPost]
+        public ActionResult Edit(collaborateurtitulaire CollT)
+        {
+            ViewBag.IDFONCTION = new SelectList(bd.fonction, "IDFONCTION", "NOMFONCTION");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    bd.Entry(CollT).State = System.Data.Entity.EntityState.Modified;
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(CollT);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Collaborateur/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            collaborateurtitulaire CollT = bd.collaborateurtitulaire.Find(id);
+            if (CollT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(CollT);
+        }
+
+        // POST: Collaborateur/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int? id, collaborateurtitulaire CollTt)
+        {
+            try
+            {
+                collaborateurtitulaire CollT = new collaborateurtitulaire();
+                if (ModelState.IsValid)
+                {
+                    if (id == null)
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                    CollT = bd.collaborateurtitulaire.Find(id);
+                    if (CollT == null)
+                        return HttpNotFound();
+                    bd.collaborateurtitulaire.Remove(CollT);
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(CollT);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ source/ProjetFinalEval/Models/AccountViewModels.cs
@@ -0,0 +1,109 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace ProjetFinalEval.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Courrier électronique")]
+        public string Email { get; set; }
+    }
+
+    public class ExternalLoginListViewModel
+    {
+        public string Action { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+
+    public class ManageUserViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe actuel")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "La chaîne {0} doit comporter au moins {2} caractères.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Nouveau mot de passe")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmer le nouveau mot de passe")]
+        [Compare("NewPassword", ErrorMessage = "Le nouveau mot de passe et le mot de passe de confirmation ne correspondent pas.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Courrier électronique")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+
+        [Display(Name = "Mémoriser le mot de passe ?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "La chaîne {0} doit comporter au moins {2} caractères.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmer le mot de passe ")]
+        [Compare("Password", ErrorMessage = "Le mot de passe et le mot de passe de confirmation ne correspondent pas.")]
+        public string ConfirmPassword { get; set; }
+
+        public int IDFONCTION { get; set; }
+        public string NOM { get; set; }
+        public string PRENOM { get; set; }
+        public byte[] IMAGE { get; set; }
+        public bool FLAGEVAL { get; set; }
+
+        public string idUser { get; set; }
+        public HttpPostedFileBase File { get; set; }
+    }
+
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Courrier électronique")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} doit contenir au moins {2} caractères.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmer le mot de passe")]
+        [Compare("Password", ErrorMessage = "Le nouveau mot de passe et le mot de passe de confirmation ne correspondent pas.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Courrier électronique")]
+        public string Email { get; set; }
+    }
+}
